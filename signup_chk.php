278d7e1a7c68bb5f3b3d2d31b6b57841cfbc0a13
meonu <yeonugim42@gmail.com>
2022-07-31 16:23:21 +0900

    signup php add

--- signup_chk.php
+++ signup_chk.php
@@ -1,17 +1,7 @@
 <?php
 
-    require_once("./connectSQL.php");
+    include_once("./connectSQL.php");
 
-    $userid = $_POST["title"];
-
-    $userpw = $_POST["userpw"];
-
-    $name = $_POST["username"];
-
-    $userphone = $_POST["userphone"];
-
-    $email = $_POST["useremail"];
-
-    echo "<h3>추가될 회원 정보는 {$username},{$userpw},{$name},{$userphone},{$email}</h3>";
+ 
 
 ?>
