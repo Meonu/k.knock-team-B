5fac035edddf0e613a2bce0bdf75d93421ab1d7f
meonu <yeonugim42@gmail.com>
2022-08-07 17:22:29 +0900

    edit

--- idcheck.php
+++ idcheck.php
@@ -13,13 +13,11 @@ ini_set("display_errors",1);
 
     $connect = new mysqli($servername, $user, $password, $DBname);
 
-    if (!$connect)
-     echo "<h2>서버와의 연결 실패</h2>";
-
-
     $userid = $_GET["userid"];
     $sql = "SELECT * FROM mem_info where userid='$userid'";
     $result = mysqli_fetch_array(mysqli_query($connect, $sql));
+    if (!$connect)
+     echo "<h2>서버와의 연결 실패</h2>";
     else if(!$result){
         echo "<span style='color:blue;'>$userid</span> 는 사용 가능한 아이디입니다.";
        ?><p><input type=button value="이 ID 사용" onclick="opener.parent.decide(); window.close();"></p>
