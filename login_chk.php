f5559fdf7979c8c0861a2ac041151f4bc0c36cfc
meonu <yeonugim42@gmail.com>
2022-08-09 18:39:14 +0900

    login function implememtation

--- login_chk.php
+++ login_chk.php
@@ -2,16 +2,35 @@
 
 $connect = new mysqli("localhost","yeonugim","kyw@514514514","MEMBER_INFO");
 
-if(!$connect)
-{
-    echo "<h2>연결을 실패하였습니다.</h2>";
-}
 
 $userid = $_POST["userid"];
 $userpw = $_POST["userpw"];
 
+$idsql = "SELECT * FROM mem_info where userid='$userid'";
+$idresult = mysqli_fetch_array(mysqli_query($connect, $idsql));
 
+$pwsql = "SELECT * FROM mem_info where userid='$userpw'";
+$pwresult = mysqli_fetch_array(mysqli_query($connect, $pwsql));
 
+if (!$connect)
+     echo "<h2>서버와의 연결 실패</h2>";
+    else if(!$idresult || !$pwresult){
+        
+        ?>
+        <script>
+        alert("아이디 또는 비밀번호가 잘못되었습니다.")
+        </script>
+        <?php
+    } 
+    else 
+    {
+        ?>
+        <script>
+        alert("로그인을 정상적으로 성공하였습니다.")
+        location.href = "index.php";
+        </script>
+        <?php
+    }
 
 
 
