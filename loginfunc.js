4bef106c7ec90a16a7f7ad1861a88862283ca9ab
meonu <yeonugim42@gmail.com>
2022-08-11 19:02:53 +0900

    edit

---
+++ loginfunc.js
@@ -0,0 +1,18 @@
+
+    function right_login()
+    {
+        var userid = document.getElementById("userid").value;
+        var userpw = document.getElementById("userpw").value;
+
+        if(userid == '')
+        {
+            alert("아이디를 입력해주세요.");
+            return false;
+        }
+        else if (userpw == '')
+        {
+            alert("비밀번호를 입력해주세요");
+            return false;
+        }
+        else return true;
+    }
