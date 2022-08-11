ad0b7ef29797066f9741f7dc0ce837159352bcbc
meonu <yeonugim42@gmail.com>
2022-08-11 18:41:27 +0900

    edit

--- signupfunc.js
+++ signupfunc.js
@@ -71,3 +71,22 @@ function check()
 	    document.getElementById("checkbutton").value = "ID 중복 검사"
 	    document.getElementById("checkbutton").setAttribute("onclick", "check()")
     }
+
+    function right_login()
+    {
+        var userid = document.getElementById("userid").value;
+        var userpw = document.getElementById("userpw").value;
+
+        if(userid == '')
+        {
+            alert("아이디를 입력해주세요.");
+            return false;
+        }
+        else if (userpw == '')
+        {
+            alert("비밀번호를 입력해주세요");
+            return false;
+        }
+        else return true;
+
+    }
