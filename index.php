420efb4546ff2c3ae7c427253222f8e4692def7d
meonu <yeonugim42@gmail.com>
2022-08-17 19:57:29 +0900

    edit

--- index.php
+++ index.php
@@ -81,6 +81,10 @@ session_start();
             <tr>
                 <td width = "70"><?php echo $no; ?></td>
                 <td width = "500"><a href=""><?php echo $title;?></a></td>
+                <td width = "120"><?php echo $board['name'];?></a></td>
+                <td width = "100"><?php echo $board['date'];?></a></td>
+                <td width = "100"><?php echo $board['thumb'];?></a></td>
+                <td width = "100"><?php echo $board['view'];?></a></td>
             </tr>
         </tbody>
         <?php } ?>
