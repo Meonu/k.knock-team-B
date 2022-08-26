4830dc6bff79ee2992ecf938a8a4c10df3507d26
meonu <yeonugim42@gmail.com>
2022-08-26 16:45:53 +0900

    reply modify system

---
+++ modreply_ok.php
@@ -0,0 +1,43 @@
+<?php
+$no = $_POST['no'];
+$content = $_POST['rep_content'];
+
+
+error_reporting(E_ALL); 
+ini_set("display_errors",1);
+
+$password = "kyw@514514514";
+
+$servername = "localhost";
+
+$user = "yeonugim";
+
+$DBname = "MEMBER_INFO";
+
+
+$connect = new mysqli($servername, $user, $password, $DBname);
+
+if (!$connect)
+ echo "<h2>서버와의 연결 실패</h2>";
+
+ $query = "update reply set content ='$content' where no = $no";
+
+
+
+if(mysqli_query($connect,$query))
+{
+   ?>
+    <script>
+    alert("게시글 등록이 완료되었습니다.");
+    </script>
+    <?php
+}
+else
+{
+    ?>
+    <script>
+    alert("데이터베이스 연동을 실패하였습니다.");
+    </script>
+    <?php
+}
+?>
