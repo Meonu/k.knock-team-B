e3a25b2cd32a5e9a2601ab40676348d3cefe4c4a
meonu <yeonugim42@gmail.com>
2022-08-30 00:20:32 +0900

    security

--- viewpost.php
+++ viewpost.php
@@ -88,7 +88,7 @@
             <td><?php echo $rows['title'] ?></td>
         </tr>
         <tr>
-            <td>파일 <a href="download.php?no=<?php echo $no ?>" download><?php echo $rows['filename']?></a></td>
+            <td>파일 <a href="download.php?no=<?php echo $no ?>"><?php echo $rows['filename']?></a></td>
         </tr>
         <tr>
             <td>작성자 <?php echo $rows['name'] ?></td>
